966700d908d1417592bd424cb0e05c6895259545
Tommy Otervik <tommyeotervik@gmail.com>
2022-03-27 07:08:29 +0200

    Create Group.java

---
+++ demo/src/main/java/com/tommyotervik/groupfinder/demo/entity/Group.java
@@ -0,0 +1,148 @@
+package com.tommyotervik.groupfinder.demo.entity;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name="groups")
+@JsonIgnoreProperties(ignoreUnknown=true)
+public class Group {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="category")
+	private String category;
+	
+	@Column(name="organizer")
+	private String organizer;
+	
+	@Column(name="title")
+	private String title;
+	
+	@Column(name="description")
+	private String description;
+	
+	@Column(name="location")
+	private String location;
+	
+	@Column(name="date")
+	private String date;
+	
+	@Column(name="time")
+	private String time;
+	
+	// TODO fiks, husk på at jeg har kommentert ut get/set for student
+	/*
+	@ManyToOne(cascade= {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+	@JoinColumn(name="StudentId")
+	private Student student;
+	*/
+	
+	public Group() {
+		
+	}
+
+	public Group(String category, String organizer, String title, String description, String location, String date,
+			String time) {
+		this.category = category;
+		this.organizer = organizer;
+		this.title = title;
+		this.description = description;
+		this.location = location;
+		this.date = date;
+		this.time = time;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public String getOrganizer() {
+		return organizer;
+	}
+
+	public void setOrganizer(String organizer) {
+		this.organizer = organizer;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public String getTime() {
+		return time;
+	}
+
+	public void setTime(String time) {
+		this.time = time;
+	}
+
+	/*
+	public Student getStudent() {
+		return student;
+	}
+
+	public void setStudent(Student student) {
+		this.student = student;
+	}
+	*/
+	
+	
+	
+	
+	
+	
+
+}
